79a76f263b7cc035c5da082820b8e163848e0ee6
Lewis <lewissngpt@gmail.com>
2021-03-17 23:51:54 +0800

    Second

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('.submit-btn');
+const error = document.querySelector('.error-msg');
+form.addEventListener('submit', handleSubmit);
+submitBtn.addEventListener('click', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+    fetchWeather();
+}
+
 async function getWeatherData(location) {
     const response = await fetch(
         `http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`,
@@ -19,6 +30,7 @@ function processData(weatherData) {
     // grab all the data i want to display on the page
     const myData = {
         condition: weatherData.current.condition.text,
+        conditionIcon: weatherData.current.condition.icon,
         feelsLike: {
             f: Math.round(weatherData.current.feelslike_f),
             c: Math.round(weatherData.current.feelslike_c),
@@ -41,4 +53,37 @@ function processData(weatherData) {
     }
 
     return myData;
+}
+
+function displayData(newData) {
+    const weatherInfo = document.getElementsByClassName('info');
+    Array.from(weatherInfo).forEach((div) => {
+        if (div.classList.contains('fade-in2')) {
+            div.classList.remove('fade-in2');
+            div.offsetWidth;
+            div.classList.add('fade-in2');
+        } else {
+            div.classList.add('fade-in2');
+        }
+    });
+
+    document.querySelector('.weather-icon').src = newData.conditionIcon;
+    document.querySelector('.condition').textContent = newData.condition;
+    document.querySelector('.location').textContent = `${newData.location}, ${newData.region}`;
+    document.querySelector('.degrees').textContent = newData.currentTemp.f;
+    document.querySelector('.feels-like').textContent = `FEELS LIKE: ${newData.feelsLike.f}`;
+    document.querySelector('.wind-mph').textContent = `WIND: ${newData.wind} MPH`;
+    document.querySelector('.humidity').textContent = `HUMIDITY: ${newData.humidity}`;
+}
+
+
+function reset() {
+    form.reset();
+}
+
+// get location from user
+function fetchWeather() {
+    const input = document.querySelector('input[type="text"]');
+    const userLocation = input.value;
+    getWeatherData(userLocation);
 }
